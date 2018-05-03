4a0d647e47c3937c4f66c6548996edb59894dd9a
Xiaotian Bai <xiaotianbai0206@gmail.com>
2018-05-03 13:09:26 -0400

    0503 modification

--- main.cpp
+++ main.cpp
@@ -1,63 +1,7 @@
 #include <iostream>
-#include "vector"
 
 using namespace std;
 
-
-class A {
-public:
-    A() { cout << "1"; }
-
-    A(const A &obj) { cout << "2"; }
-};
-
-class B : virtual A {
-public:
-    B() { cout << "3"; }
-
-    B(const B &obj) { cout << "4"; }
-};
-
-class C : virtual A {
-public:
-    C() { cout << "5"; }
-
-    C(const C &obj) { cout << "6"; }
-};
-
-class D : public B, public C {
-public:
-    D() { cout << "7"; }
-
-    D(const D &obj) { cout << "8"; }
-
-    ~D() {}
-};
-
-class X {
-private:
-    int i;
-    int *pi;
-public:
-    X()
-            : pi(new int) {}
-
-    X(const X &copy)   // <-- copy ctor
-            : i(copy.i), pi(new int(*copy.pi))  // <-- note this line in particular!
-    {}
-};
-
-int const s = 9;
-
-int x[100];
-
 int main() {
-    vector<int> v{7, 8, 6, 5, 44, 8, 3, 2};
-    vector<int> v1(v.begin(), v.begin());
-//    quicksort :: qsort(v, 0, 7);
-//    for(vector<int> :: iterator i = v.begin(); i != v.end(); ++i) {
-//        cout<<*i;
-//    }
-    string a = "abc";
-    cout << *(a.end() - 1);
+
 }
